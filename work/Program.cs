d2f0951d32f6af7ff307a881ed318042a9c025e6
Boss <madelkhan@gmail.com>
2022-08-15 15:51:01 +0600

    final step

--- work/Program.cs
+++ work/Program.cs
@@ -1,7 +1,18 @@
 ﻿using System;
-class Script {
+class Script 
+{
   static void Main() {
     string[] arr_read = {"hello","2","world",":-)"};
+    
+    if (arr_read.Length == 0) {
+        int arr_length = System.Convert.ToInt32(Console.ReadLine());
+        arr_read = new string[arr_length];
+        
+        for (int i = 0; i < arr_length; i++) {
+            arr_read[i] = Console.ReadLine();
+        }
+    }
+    
     string[] arr_write = {};
     int count = 0;
     
