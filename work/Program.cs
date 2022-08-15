0d0cf2fe372cb73a778d24f5b3252241b1b73458
Boss <madelkhan@gmail.com>
2022-08-15 15:48:13 +0600

    step 3

--- work/Program.cs
+++ work/Program.cs
@@ -2,8 +2,17 @@
 class Script {
   static void Main() {
     string[] arr_read = {"hello","2","world",":-)"};
-    string[] arr_write = new string[arr_read.Length];
+    string[] arr_write = {};
     int count = 0;
+    
+    for (int i = 0; i < arr_read.Length; i++) {
+        if (arr_read[i].Length <= 3) {
+            count += 1;
+        }
+    }
+    
+    arr_write = new string[count];
+    count = 0;
     
     for (int i = 0; i < arr_read.Length; i++) {
         if (arr_read[i].Length <= 3) {
