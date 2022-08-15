4891311c0f7f31a5be7583a57234c0b01c475d45
Boss <madelkhan@gmail.com>
2022-08-15 15:46:43 +0600

    step 2

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Script {
+  static void Main() {
+    string[] arr_read = {"hello","2","world",":-)"};
+    string[] arr_write = new string[arr_read.Length];
+    int count = 0;
+    
+    for (int i = 0; i < arr_read.Length; i++) {
+        if (arr_read[i].Length <= 3) {
+            arr_write[count] = arr_read[i];
+            Console.WriteLine(arr_write[count]);
+            count += 1;
+        }
+    }
+  }
+}
